2a6f5fb7d59afc426cbd032319df59cd9e21ed83
AliAbdelbaky <ali.abdelbaky2000@gmail.com>
2025-03-17 23:11:06 +0200

    opt

--- src/middleware/protected.ts
+++ src/middleware/protected.ts
@@ -1,9 +1,10 @@
 import {getToken, destroyToken} from '@/services/jwtService.ts';
 import {useAuthStore} from '@/store/auth.ts';
+import type {RouteLocationNormalized, RouteLocationNormalizedLoaded} from "vue-router";
 
-export default async function (to, from, next) {
+export default async function (to: RouteLocationNormalized, from: RouteLocationNormalizedLoaded, next: (str?: string) => void) {
 
-    console.log("Protected Middleware", to, from, next);
+    console.log("Protected Middleware", to, from, next)
     const authStore = useAuthStore();
     const token = getToken();
 
